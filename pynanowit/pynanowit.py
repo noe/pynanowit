c7e538f3b09fe48ef2aa42cd68b7d6cd3b2fccf2
Matheus Santos <matheus.santos@struct.com.br>
2016-11-29 20:46:36 -0200

    Fixing problems to deploy

--- pynanowit/pynanowit.py
+++ pynanowit/pynanowit.py
@@ -19,7 +19,7 @@ from werkzeug import check_password_hash, generate_password_hash
 
 
 # configuration
-DATABASE = '/tmp/pynanowit.db'
+DATABASE = './pynanowit.db'
 PER_PAGE = 30
 DEBUG = True
 SECRET_KEY = 'development key'
