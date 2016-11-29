ebfeef10bcbbad6c74f8c9ae4d806f4179494368
Matheus Santos <matheus.santos@struct.com.br>
2016-11-29 20:18:41 -0200

    Fixing problems to deploy

--- setup.py
+++ setup.py
@@ -4,13 +4,7 @@ setup(
     name='pynanowit',
     packages=['pynanowit'],
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
-    setup_requires=[
-        'pytest-runner',
-    ],
-    tests_require=[
-        'pytest',
-    ],
+    install_requires=['flask'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
